888e833ff67a865ba5540df02afc1aa492216e3e
Jim DeLois <jimdelois@users.noreply.github.com>
2016-06-09 16:07:30 -0400

    Adding MapperFactory Class and Tests

--- src/Configuration/MapperFactory.php
+++ src/Configuration/MapperFactory.php
@@ -2,10 +2,48 @@
 
 namespace Improv\Configuration;
 
+use Improv\Configuration\Exceptions\InvalidMapperClassException;
+
 class MapperFactory
 {
+    /**
+     * @var string
+     */
+    private $mapper_class;
+
+    /**
+     * @param string $mapper_class
+     *
+     * @throws InvalidMapperClassException
+     */
+    public function __construct($mapper_class = Mapper::class)
+    {
+        $this->mapper_class = $mapper_class;
+
+        if ($mapper_class === Mapper::class) {
+            return;
+        }
+
+        if (!class_exists($mapper_class)) {
+            throw new InvalidMapperClassException(
+                sprintf('Provided Mapper class "%s" does not exist.', $mapper_class)
+            );
+        }
+
+        if (!is_subclass_of($mapper_class, Mapper::class)) {
+            throw new InvalidMapperClassException(
+                sprintf('Provided Mapper class "%s" must extend "%s".', $mapper_class, Mapper::class)
+            );
+        }
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return Mapper
+     */
     public function createNew($value)
     {
-        return new Mapper($value);
+        return new $this->mapper_class($value);
     }
 }
